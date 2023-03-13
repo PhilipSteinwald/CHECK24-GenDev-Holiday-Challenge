d7be186d697a31ae891f924c093b63a0de938be1
ge95has <philip.steinwald@outlook.de>
2023-03-13 16:17:24 +0100

    index-Page + templates structure

--- holiday_project/members/views.py
+++ holiday_project/members/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+  return render(request, 'index.html')
+
+def resp(request):
+  na = (str(request).split('?')[1])
+  context = {'test_list': na[0:(len(na)-2)]}
+  return render(request, 'response.html', context=context)
